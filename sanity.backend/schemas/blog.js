f3bfbcd3c63bc4d97e4dbd39093ab43f16dacc90
Muhammad Hassan Naeem <nhassan_1990@yahoo.com>
2022-07-25 22:44:07 +0500

    created and fetched blogs from snaity

--- sanity.backend/schemas/blog.js
+++ sanity.backend/schemas/blog.js
@@ -4,25 +4,94 @@ export default {
   title: "Blog",
   fields: [
     {
-      name: "name",
+      name: "title",
       type: "string",
       title: "Title",
     },
     {
-      name: "content",
-      type: "string",
-      title: "Content",
+      title: "Slug",
+      name: "slug",
+      type: "slug",
+      options: {
+        source: "title",
+        maxLength: 200, // will be ignored if slugify is set
+        slugify: (input) =>
+          input.toLowerCase().replace(/\s+/g, "-").slice(0, 200),
+      },
     },
     {
-      title: "Launch Scheduled At",
-      name: "launchAt",
-      type: "datetime",
-      option: {
-        dateFormat: "YYYY-MM-DD",
-        timeFormat: "HH:mm",
-        timeStep: 5,
-        calendarTodayLabel: "Today",
+      name: "content",
+      title: "Content",
+      type: "array",
+      of: [
+        {
+          type: "block",
+        },
+        {
+          type: "image",
+          fields: [
+            {
+              type: "text",
+              name: "alt",
+              title: "Alternative text",
+              description: `Some of your visitors cannot see images, 
+                  be they blind, color-blind, low-sighted; 
+                  alternative text is of great help for those 
+                  people that can rely on it to have a good idea of 
+                  what\'s on your page.`,
+              options: {
+                isHighlighted: true,
+              },
+            },
+          ],
+        },
+      ],
+    },
+    {
+      name: "metadesc",
+      type: "string",
+      title: "Meta Description",
+    },
+    {
+      title: "Blog Image",
+      name: "blogimage",
+      type: "image",
+      options: {
+        hotspot: true, // <-- Defaults to false
       },
+      fields: [
+        {
+          name: "caption",
+          type: "string",
+          title: "Caption",
+          options: {
+            isHighlighted: true, // <-- make this field easily accessible
+          },
+        },
+        {
+          // Editing this field will be hidden behind an "Edit"-button
+          name: "attribution",
+          type: "string",
+          title: "Attribution",
+        },
+      ],
+    },
+    {
+      title: "Created At",
+      name: "createdAt",
+      type: "datetime",
+    },
+    {
+      name: "author",
+      type: "object",
+      fields: [
+        {
+          title: "Author",
+          name: "author",
+          type: "reference",
+          to: [{ type: "author" }],
+        },
+      ],
     },
   ],
 };
